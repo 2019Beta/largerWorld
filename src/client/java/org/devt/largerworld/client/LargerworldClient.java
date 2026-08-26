a6a531124042f2561e0dcc11a7591909d2218604
2019Beta <3285118962@qq.com>
2026-08-26 16:26:53 +0800

    init

---
+++ src/client/java/org/devt/largerworld/client/LargerworldClient.java
@@ -0,0 +1,10 @@
+package org.devt.largerworld.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LargerworldClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/devt/largerworld/Largerworld.java
@@ -0,0 +1,10 @@
+package org.devt.largerworld;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Largerworld implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
